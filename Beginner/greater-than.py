5e1b2a8cf3774257c0b8c20e9319af9b6c668147
Marcos Melo <marcaos.melo@gmail.com>
2022-01-19 18:30:34 +0000

    Comparison greater than

---
+++ Beginner/greater-than.py
@@ -0,0 +1,9 @@
+print("O maior de 2 números, digite a seguir: \n")
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+maior = 0
+if x > y:
+    maior = x
+else:
+    maior = y
+print("a- O maior dos 2 números é -> ", maior)
