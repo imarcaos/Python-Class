ed6517aab6501a19f451d54e09569243b40ca1c5
Marcos Melo <marcaos.melo@gmail.com>
2022-01-20 13:21:05 +0000

    Create sum-number-from-list.py

---
+++ Beginner/sum-number-from-list.py
@@ -0,0 +1,11 @@
+# Soma os números contidos em uma lista - 20/02/2022
+
+x = [5,10,15,20,25,30]
+listSize = len(x)
+sumList = 0
+
+while(listSize > 0):
+    sumList += x[listSize-1]
+    listSize -= 1
+
+print("A Soma dos números contidos na lista é -> ", sumList)
