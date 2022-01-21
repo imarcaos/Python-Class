e4b73171b4e2affc87635bbb318cf20deb1659bc
Marcos Melo <marcaos.melo@gmail.com>
2022-01-21 18:04:24 +0000

    Create body-mass-index.py
    Calculator BMI - Calculadora IMC

---
+++ Beginner/body-mass-index.py
@@ -0,0 +1,12 @@
+# Calculator BMI (Body Mass Index) ou Calculadore IMC (Índice de Massa Corporal)
+# Peso KG , Altura CM
+#21/01/2022
+
+
+altura = float(input("Qual é a sua Altura em cm ? "))
+peso = float(input("Qual é o seu peso em kg ? "))
+
+print(altura, peso)
+#imc = peso / altura * altura
+
+
