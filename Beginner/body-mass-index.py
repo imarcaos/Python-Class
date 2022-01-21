0681525602cba6c3988e0c7a42209c8200e6d3a9
Marcos Melo <marcaos.melo@gmail.com>
2022-01-21 18:49:13 +0000

    Update body-mass-index.py

--- Beginner/body-mass-index.py
+++ Beginner/body-mass-index.py
@@ -2,11 +2,21 @@
 # Peso KG , Altura CM
 #21/01/2022
 
-
 altura = float(input("Qual é a sua Altura em cm ? "))
 peso = float(input("Qual é o seu peso em kg ? "))
 
-print(altura, peso)
-#imc = peso / altura * altura
+imc = peso / (altura/100)**2 
+# Para formatar as casas decimais utilizar após a variável :.2f
+#print(imc)
+print("\n-----------------------------------------------------------------------")
 
-
+if imc < 18.5:
+    print(f"Seu IMC é de {imc:.2f} , e é classificado como Magreza\n")
+elif imc >= 18.5 and imc < 25:
+    print(f"Seu IMC é de {imc:.2f} , e é classificado como Normal\n")
+elif imc >= 25 and imc < 30:
+    print(f"Seu IMC é de {imc:.2f} , e é classificado como Sobrepeso\n")
+elif imc >= 30 and imc < 40:
+    print(f"Seu IMC é de {imc:.2f} , e é classificado como Obesidade\n")
+else:
+    print(f"Seu IMC é de {imc:.2f} , e é classificado como Obesidade Grave\n")
