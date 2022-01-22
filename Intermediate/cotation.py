d1f9d4a117cc8613b20dd07ac82839e7ff49e7e3
Marcos Melo <marcaos.melo@gmail.com>
2022-01-22 16:25:46 +0000

    Update cotation.py

--- Intermediate/cotation.py
+++ Intermediate/cotation.py
@@ -1,4 +1,5 @@
 # Retorna a cotação de algumas moedas, programa somente para testar o Tkinter - desenho de janelas GUI
+# ! Estudar como tornar em um executável
 # 22/01/2022
 
 import requests
@@ -17,17 +18,22 @@ def pegar_cotacoes():
     Dólar/EUR: {cotacao_dolar}
     BTC/Dólar: {cotacao_btc}'''
 
-    print(texto)
+    #print(texto)
+    texto_cotacoes["text"] = texto
 
 #pegar_cotacoes()
 
 janela = Tk() # cria a janela
 janela.title("Cotação Atual das Moedas")
+# janela.geometry("350x250")
 
 texto_orientacao = Label(janela, text="Clique no Botão para Atualizar as Cotações das Moedas")
-texto_orientacao.grid(column=0, row=0)
+texto_orientacao.grid(column=0, row=0, padx=10, pady=10)
 
-botao = Button(janela, text="ATUALIZAR COTAÇÕES", command=pegar_cotacoes)
-botao.grid(column=0, row=1)
+botao = Button(janela, text="ATUALIZAR COTAÇÕES", command=pegar_cotacoes) # não colocar parenteses na função para evitar exec.autom.
+botao.grid(column=0, row=1, padx=10, pady=10)
+
+texto_cotacoes = Label(janela, text="")
+texto_cotacoes.grid(column=0, row=2, padx=10, pady=10)
 
 janela.mainloop() # mantém a janela aberta e deve estar sempre em último
