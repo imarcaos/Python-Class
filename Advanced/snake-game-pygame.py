b8d09564ed6ec174e75393d9aeafd2cac980e543
Marcos Melo <marcaos.melo@gmail.com>
2022-01-25 10:54:24 +0000

    Update snake-game-pygame.py

--- Advanced/snake-game-pygame.py
+++ Advanced/snake-game-pygame.py
@@ -9,6 +9,17 @@ from turtle import down
 import pygame, random
 from pygame.locals import *
 
+# Função para alinhar as casas decimais da maçã
+def on_grid_random():
+    x = random.randint(0, 590)
+    y = random.randint(0, 590)
+    return (x//10 * 10, y//10 * 10) # Ao fazer esta divisao excluo unidade e deixo as decimais, para ficar alinhado
+
+# Função de colisão
+def collision (c1, c2):
+    return (c1[0] == c2[0] and (c1[1] == c2[1]))
+
+
 UP = 0
 RIGHT = 1
 DOWN = 2
@@ -22,7 +33,8 @@ snake = [(200, 200), (210, 200), (220, 200)]
 snake_skin = pygame.Surface((10, 10))
 snake_skin.fill((255, 255, 255)) # branco
 
-apple_pos = (random.randint(0, 590), random.randint(0, 590)) # 590 ultima posição sem sair da tela 600 com cada quadrado de 10
+# apple_pos = (random.randint(0, 590), random.randint(0, 590)) # 590 ultima posição sem sair da tela 600 com cada quadrado de 10
+apple_pos = on_grid_random() # comparando com a linha acima, esta alinha a maçã nas casas em multiplos de 10
 apple = pygame.Surface((10, 10))
 apple.fill((255, 0, 0)) # vermelho
 
@@ -49,7 +61,13 @@ while True:
                 my_direction = RIGHT
 
 
+    if collision(snake[0], apple_pos):
+        apple_pos = on_grid_random() # nova posição para a maçã
+        snake.append((0, 0)) # adiciona um novo item ao final da lista (+ 1 pedaço da calda)
 
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+        
     if my_direction == UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)    
     if my_direction == DOWN:
@@ -59,8 +77,7 @@ while True:
     if my_direction == LEFT:
         snake[0] = (snake[0][0] - 10, snake[0][1])
 
-    for i in range(len(snake) - 1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
+
 
 
     screen.fill((0, 0, 0)) # antes de plotar, precisamos sempre limpar a tela
