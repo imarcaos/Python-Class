fbebbf6379d57b281a7884711e4edd1905786963
Marcos Melo <marcaos.melo@gmail.com>
2022-01-28 21:49:22 +0000

    Assistente Virtual
    Projeto de Assistente Virtual iniciado no Windows, visto no Linux estar com erros na bibllioteca pyaudio

---
+++ Advanced/virtual-assistant.py
@@ -0,0 +1,36 @@
+#
+# Bibliotecas: SpeechRecognition , pyttsx3 , PyAudio
+# PyAudio no Windows : >>> pip install pipwin  and  >>> pipwin install pyaudio
+# 28/01/2022
+
+import speech_recognition as sr
+import pyttsx3, datetime
+
+audio = sr.Recognizer()
+maquina = pyttsx3.init()
+
+def executa_comando():
+    try:
+        with sr.Microphone() as source:
+            print('Ouvindo...')
+            voz = audio.listen(source)
+            comando = audio.recognize_google(voz, language='pt-BR')
+            comando = comando.lower() # todo comando de voz é convertido para letra minúscula
+            if 'com' in comando:
+                comando = comando.replace('com', '')
+                maquina.say(comando)
+                maquina.runAndWait()
+
+    except:
+        print('Microfone não está ok')
+
+    return comando
+
+def comando_voz_usuario():
+    comando = executa_comando()
+    if 'horas' in comando:
+        hora = datetime.datetime.now().strftime('%H:%M')
+        maquina.say('Agora são' + hora)
+        maquina.runAndWait()
+
+comando_voz_usuario()
